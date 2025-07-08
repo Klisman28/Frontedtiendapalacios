279863af1e89fba8e46e3ef023c6790d6091d563
Klisman <108555545+klisman11@users.noreply.github.com>
2025-07-08 13:18:45 -0600

    error en peticiones

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,7 +2,7 @@ import ApiService from './ApiService'
 
 export async function apiSignIn (data) {
     return ApiService.fetchData({
-        url: '/auth/sing-in',
+        url: '/auth/sign-in',
         method: 'post',
         data
     })
